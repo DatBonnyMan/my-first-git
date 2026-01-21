6245c274d289487906813984432337f9d890c7bf
DatBonnyMan <tij46@yahoo.com>
2026-01-21 06:30:23 +0100

    Add dark mode feature with toggle button

--- js/app.js
+++ js/app.js
@@ -12,3 +12,7 @@ function toggleDarkMode() {
     document.body.classList.toggle('dark-mode');  
     localStorage.setItem('darkMode', document.body.classList.contains('dark-mode'));  
 }  
+function toggleDarkMode() { 
+    document.body.classList.toggle('dark-mode'); 
+    localStorage.setItem('darkMode', document.body.classList.contains('dark-mode')); 
+} 
